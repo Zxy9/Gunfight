09028b48b198941486e2de9d5d8e43283247c992
DESKTOP-TCP6R2K\ds <1402013742@qq.com>
2020-11-08 12:32:23 +0800

    GunFactoryAndPropFactory

---
+++ Assets/Scenes/GunFactory/Gun1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    public class Gun1:GunFactory
+    {
+    public void Start()
+    {
+        ShootMutiple(2,2);
+    }
+    public void Update()
+    {
+        Shoot();
+    }
+
+}

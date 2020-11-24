42ebb542b8ce1647fc7072bf632a9faaf12e19f1
PC-20201002IMTW\Administrator <401941971@qq.com>
2020-11-24 10:45:36 +0800

    更新场景一

--- Assets/Script/PosDirection.cs
+++ Assets/Script/PosDirection.cs
@@ -8,6 +8,8 @@ public class PosDirection : MonoBehaviour
     public GameObject Particlefeb;
     Vector3 actorXZ;
     Vector3 ParticlefebXZ;
+    public Animator KaimenA;
+    public GameObject warningG;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +21,8 @@ public class PosDirection : MonoBehaviour
     {
         if (ReachPos() == true)
         {
-            Debug.Log("222");
+            KaimenA.enabled = true;
+            warningG.SetActive(true);
         }
       
     }

61b198f2ef6e5cc0af88409628df135990ec23d5
PC-20201002IMTW\Administrator <401941971@qq.com>
2020-11-25 15:07:00 +0800

    开始界面

--- Assets/Script/GameControl/AudioControl.cs
+++ Assets/Script/GameControl/AudioControl.cs
@@ -1,18 +1,28 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AudioControl : MonoBehaviour
 {
     // Start is called before the first frame update
+    AudioSource audi;
     void Start()
     {
-        
+        audi = gameObject.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void AudioStart()
+    {
+        audi.Play();
+    }
+    public void AudioStop()
+    {
+        audi.Stop();
     }
 }
--- Assets/Script/GameControl/GameSetting.cs
+++ Assets/Script/GameControl/GameSetting.cs
@@ -1,18 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class GameSetting : MonoBehaviour
 {
     // Start is called before the first frame update
+    public AudioSource audios;
+
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void SilOff()
+    {
+        audios.mute = !audios.mute;
     }
 }
